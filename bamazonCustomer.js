4c72128bbf3f8d8f4f5415e434e424fdfbf3826a
Claud Pod <claudiapollinger.cp@gmail.com>
2019-07-11 17:56:37 -0600

    putting all products in one function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,10 +4,12 @@ var mysql = require("mysql");
 
 var inquirer = require("inquirer");
 
+require ("console.table");
+
 var connection = mysql.createConnection({
     user: "root",
     password: "root",
-    host: "bamazonhw",
+    host: "localhost",
     port: 8889,
     database: "bamazon_db"
 });
@@ -22,30 +24,75 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    startBamazon();
+    showAll();
 });
 
-function startBamazon() {
-    inquirer.prompt({
-        type: "list",
-        name: "choicesoption",
-        message: "Come check out the amazing products at Bamazon, which product would you liek to search first?",
-        choices: ["Shop by the department", "Shop by the product"]
+// function startBamazon() {
+//     inquirer.prompt({
+//         type: "list",
+//         name: "choicesoption",
+//         message: "Come check out the amazing products at Bamazon, which product would you like to search first?",
+//         choices: ["Shop by the department", "Shop by the product"]
 
-    }).then(function (response) {
-        switch (response.choicesoption) {
-            case "Shop by the department":
-                return shopDept();
+//     }).then(function (response) {
+//         switch (response.choicesoption) {
+//             case "Shop by the department":
+//                 return shopDept();
 
-            case "Shop by the product":
-                return shopProduct();
+//             case "Shop by the product":
+//                 return shopProduct();
 
-            default:
-                connection.end();
+//             default:
+//                 connection.end();
 
-        };
-    });
+//         };
+//     });
+// }
+
+function showAll (){
+    connection.query("SELECT * FROM products", function(err,data){
+        if (err) throw err;
+
+        console.table(data);
+        
+        inquirer.prompt({
+            type:"number",
+            name:"Id_to_buy",
+            message: " The ID of the product you would like to buy?"
+
+        }).then(function(response){
+            connection.query("SELECT * FROM products WHERE ?", {id: response.Id_to_buy}, function(err,res){
+                if (err) throw err; 
+
+                if(res.length === 0){
+                    console.log(`Please enter a valid product id`)
+
+                }
+            inquirer.prompt({
+                type:"number",
+                name:"Units_to_buy",
+                message:"How many units of the product would you like to buy?"
+            }).then(function(responsetwo){
+                connection.query("SELECT stock_quantity FROM products WHERE ?", {id: response.Id_to_buy}, function(err,res2){
+                    if (err) throw err;
+                    console.log(res);
+                    
+                    console.log(res[0].stock_quantity);
+
+                    
+
+                    // check to see that responsetwo.Units_to_buy is less than or equal to res[0].stock_quantity if
+                    //needs to update the stock quantity
+                    // tell the user the purchase  and the total x amount of things 
+                    //  purchase total
+                    // connection.end();
+            })
+        })   
+            })
+        })
+    })
 }
+
 
 function shopDept() {
     connection.query(" SELECT * FROM products", function (err,data){
@@ -64,8 +111,15 @@ function shopDept() {
                    return department.indexOf(item) >= index;
                 });
             }
-        }).then(function(reponse){
-            console.log("Ive made it this far")
+        }).then(function(response){
+            console.log(response);
+            // ///// should I have a query here ?????????
+            connection.query("SELECT * FROM products WHERE ?", {department_name: response.Department}, function (err,res){
+                if (err) throw err;
+
+                 console.table(res);
+                
+            })
         })
 
 
@@ -79,3 +133,6 @@ function shopProduct() {
 // function buyProduct(){
 }
 // 
+
+
+
