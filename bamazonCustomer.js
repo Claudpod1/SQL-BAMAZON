3c2e7df2082dd07ee0c5283e04aa4a7fd9b6f057
Claud Pod <claudiapollinger.cp@gmail.com>
2019-07-11 13:09:50 -0600

    adding to functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,15 +7,75 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     user: "root",
     password: "root",
-    host:"bamazonhw",
+    host: "bamazonhw",
     port: 8889,
     database: "bamazon_db"
 });
 
-function menu () {
-    console.log("menu goes here!");
-}
-connection.connect(function(){
- console.log(`Connected as id ${connection.threadId}`)
-    menu();
+// function menu () {
+//     console.log("menu goes here!");
+// }
+// connection.connect(function(){
+//  console.log(`Connected as id ${connection.threadId}`)
+//     menu();
+// });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    startBamazon();
 });
+
+function startBamazon() {
+    inquirer.prompt({
+        type: "list",
+        name: "choicesoption",
+        message: "Come check out the amazing products at Bamazon, which product would you liek to search first?",
+        choices: ["Shop by the department", "Shop by the product"]
+
+    }).then(function (response) {
+        switch (response.choicesoption) {
+            case "Shop by the department":
+                return shopDept();
+
+            case "Shop by the product":
+                return shopProduct();
+
+            default:
+                connection.end();
+
+        };
+    });
+}
+
+function shopDept() {
+    connection.query(" SELECT * FROM products", function (err,data){
+        if (err) throw err;
+         
+        inquirer.prompt({
+            type:"list",
+            name: "Department",
+            message: "What department would you like to shop from?",
+            choices: function(){
+                var department = data.map(function(product){
+                    return product.department_name
+                });
+                
+                return department.filter (function (item, index){
+                   return department.indexOf(item) >= index;
+                });
+            }
+        }).then(function(reponse){
+            console.log("Ive made it this far")
+        })
+
+
+})
+
+
+function shopProduct() {
+
+}
+
+// function buyProduct(){
+}
+// 
